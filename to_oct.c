f56c75c982e1bb69177b970c25bb4ba4056917ad
newisdartagnan <newisboris@gmail.com>
2022-10-18 14:40:00 -0700

    decimal to octal

---
+++ to_oct.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdarg.h>
+#include <string.h>
+/**
+ * dec_to_oct - concerts a deximal to octal
+ * @n: the decimal to be converted
+ * Return: the length of the octal string
+ */
+int dec_to_oct(unsigned int n)
+{
+	long quotient, octalnum = 0;
+	char number[100];
+	int i = 1, j, l;
+
+	quotient = n;
+	l = strlen(number);
+
+	while (quotient != 0)
+	{
+		number[i] = quotient % 8;
+		quotient = quotient / 8;
+		i++;
+	}
+	for (j = i - 1; j > 0; j--)
+		octalnum *= 10 + number[j];
+	return (l);
+}
